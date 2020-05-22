bb2643140c77cb2503ff624a085188c6ec11f72e
Marina <marina.vyskirka@gmail.com>
2020-05-22 13:34:48 +0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -91,4 +91,51 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   setClock('.timer', deadLine);
+
+  // Modal
+  const modal = document.querySelector('.modal'),
+        modalOpen = document.querySelectorAll('[data-modal]'),
+        modalClose = document.querySelectorAll('[data-close');
+  
+  function openModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+    clearTimeout(modalTimerId);
+  }
+
+  function closeModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+  }
+
+  modalOpen.forEach((elem) => {
+    elem.addEventListener('click', openModal);
+  });
+
+  modalClose.forEach((elem) => {
+    elem.addEventListener('click', closeModal);
+  });
+
+  modal.addEventListener('click', (e) => {
+    if(e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 3000);
+
+  function showModalByScroll() {
+    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
